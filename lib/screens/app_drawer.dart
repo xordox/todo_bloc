00557665b6012616e5e2ea1b1f3f6a3c71d5bc09
Prabin Maharjan <coolprabin2000@gmail.com>
2023-02-01 21:15:23 +0545

    task length in drawer

--- lib/screens/app_drawer.dart
+++ lib/screens/app_drawer.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:todo_bloc/blocs/bloc_barrier.dart';
 import 'package:todo_bloc/screens/recycle_bin.dart';
 import 'package:todo_bloc/screens/tasks_screen.dart';
 
 class AppDrawer extends StatelessWidget {
-  const AppDrawer({ Key? key }) : super(key: key);
+  const AppDrawer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,22 +16,32 @@ class AppDrawer extends StatelessWidget {
               width: double.infinity,
               padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 20),
               color: Colors.grey,
-              child: Text("Task Drawer",
-              style: Theme.of(context).textTheme.headline5,),
-            ),
-            GestureDetector(
-              onTap: ()=>Navigator.of(context).pushNamed(TasksScreen.id),
-              child: const ListTile(
-                leading: Icon(Icons.folder_special,),
-                title: Text("My Tasks"),
-                trailing: Text("0"),
+              child: Text(
+                "Task Drawer",
+                style: Theme.of(context).textTheme.headline5,
               ),
+            ),
+            BlocBuilder<TaskBloc, TaskState>(
+              builder: (context, state) {
+                return GestureDetector(
+                  onTap: () => Navigator.of(context).pushNamed(TasksScreen.id),
+                  child:  ListTile(
+                    leading: const Icon(
+                      Icons.folder_special,
+                    ),
+                    title: const Text("My Tasks"),
+                    trailing: Text("${state.allTasks.length}"),
+                  ),
+                );
+              },
             ),
             Divider(),
             GestureDetector(
-              onTap: ()=> Navigator.of(context).pushNamed(RecycleBin.id),
+              onTap: () => Navigator.of(context).pushNamed(RecycleBin.id),
               child: const ListTile(
-                leading: Icon(Icons.delete,),
+                leading: Icon(
+                  Icons.delete,
+                ),
                 title: Text("Bin"),
                 trailing: Text("0"),
               ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:todo_bloc/main.dart';
+import 'package:todo_bloc/services/app_router.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( MyApp(appRouter: AppRouter(),));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
